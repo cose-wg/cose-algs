772da457d79cebb28e185673f719fa607d55d2d5
Jim Schaad <ietf@augustcellars.com>
2015-10-10 16:55:37 -0700

    Build Changes
    Don't run rake until things get ironed out
    Update my internal build script
    Do some more fill outs on initial text of draft.

--- a.bat
+++ a.bat
@@ -1,5 +1,5 @@
-c:\tools\python27\python.exe c:\tools\python27\scripts\xml2rfc draft-ietf-cose-algs.xml -o cose.txt --text
-c:\tools\python27\python.exe c:\tools\python27\scripts\xml2rfc draft-ietf-cose-algs.xml -o cose.html --html
-c:\tools\python27\python.exe c:\tools\python27\scripts\xml2rfc draft-ietf-cose-algs.xml -o cose.xml --exp
+c:\tools\python27\python.exe c:\tools\python27\scripts\xml2rfc draft-ietf-cose-alg.xml -o cose.txt --text
+c:\tools\python27\python.exe c:\tools\python27\scripts\xml2rfc draft-ietf-cose-alg.xml -o cose.html --html
+c:\tools\python27\python.exe c:\tools\python27\scripts\xml2rfc draft-ietf-cose-alg.xml -o cose.xml --exp
 
-call rake verify
+rem call rake verify
